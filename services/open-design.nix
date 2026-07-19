259d58568389e83496dd21603344769cfbe9cbdf
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-07-19 19:34:14 +0700

    fix(open-design): route nginx to bundled frontend

--- services/open-design.nix
+++ services/open-design.nix
@@ -6,7 +6,14 @@
     locations."/" = {
       proxyPass = "http://127.0.0.1:5174";
       proxyWebsockets = true;
-      extraConfig = config.homelab.nginx.privateAllow;
+      recommendedProxySettings = false;
+      extraConfig = config.homelab.nginx.privateAllow + ''
+        proxy_set_header Host 127.0.0.1:5174;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+      '';
     };
   };
 }
