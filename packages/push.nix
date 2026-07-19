42f1e14e68336b36e6f1a575d8e1910eaa060c24
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2026-07-19 23:22:16 +0700

    fix(push): use upstream release binary

--- packages/push.nix
+++ packages/push.nix
@@ -1,27 +1,28 @@
 {
   lib,
-  rustPlatform,
-  fetchFromGitHub,
-  git,
+  stdenvNoCC,
+  fetchurl,
 }:
-rustPlatform.buildRustPackage {
+stdenvNoCC.mkDerivation {
   pname = "push";
-  version = "0.8.0-unstable-2026-07-19";
+  version = "0.8.0";
 
-  src = fetchFromGitHub {
-    owner = "owainlewis";
-    repo = "push";
-    rev = "00d64d843fd28c5bc315ce8fa09fad6c952afe96";
-    hash = "sha256-+wAey9r8oq3Clar7AegO/m/be4x+U+ZkTtng9dP4n0I=";
+  src = fetchurl {
+    url = "https://github.com/owainlewis/push/releases/download/v0.8.0/push-v0.8.0-x86_64-unknown-linux-gnu.tar.gz";
+    hash = "sha256-T2EI2qLYymJwYoRpGXjdtnnXyd0mLduUsXIISbNOxfg=";
   };
 
-  cargoHash = "sha256-fkawKcPzxMW3VUuntvSnOgd6eKT6QAozQ2ysU5ORWeU=";
-  nativeBuildInputs = [ git ];
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 push -t $out/bin
+    runHook postInstall
+  '';
 
   meta = {
     description = "Personal assistant messaging gateway";
     homepage = "https://github.com/owainlewis/push";
     license = lib.licenses.mit;
     mainProgram = "push";
+    platforms = [ "x86_64-linux" ];
   };
 }
